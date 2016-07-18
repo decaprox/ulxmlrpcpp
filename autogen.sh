a34bfb68ae4c2bb5bea4af8841bb0bdd6b23aed5
Nikolay Golikov <decaprox@gmail.com>
2016-07-18 17:38:32 +0300

    Autotools files cleanup

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-case `uname` in
-    FreeBSD)
-        PATH=/usr/local/gnu-autotools/bin:$PATH
-        ;;
-esac
-
-autoreconf -i -f &&
-./configure --enable-debug -C $@
+autoreconf -i -f
